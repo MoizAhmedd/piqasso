99c5577220971b9375775429b7aa3aae0e4a7216
Thayallan-s <thayallan.srinathan@gmail.com>
2019-02-28 04:15:05 -0500

    dunzo

--- helloworld/Stepper/Stepper.py
+++ helloworld/Stepper/Stepper.py
@@ -1,4 +1,4 @@
-import attr, random, re
+import attr, random
 import math
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, execute
 from qiskit_aqua import get_aer_backend
@@ -21,9 +21,10 @@ class Stepper(object):
     """
     def new_phrase(self, model, eol, punc):
         # all random bits are best done as functions so I can mock them
-        start_key = self.qrandom(set(open(model.keys().read().split())))
-        print(start_key)
+        random = self.qrandom(len(list(model.keys())))
+        start_key = list(model.keys())[random]
         phrase = start_key
+
 
         prev_words = start_key.split()
         next_word = ''
@@ -47,7 +48,9 @@ class Stepper(object):
         string: A complete sequence constructed from the model
     """
     def new_set_length_sequence(self, model, steps):
-        start_key = self.qrandom(set(open(model.keys().read().split())))
+        #print(type(model))
+        random = self.qrandom(len(list(model.keys())))
+        start_key = list(model.keys())[random]
         sequence = start_key
 
         prev_tokens = start_key.split()
@@ -74,7 +77,8 @@ class Stepper(object):
         string: the input phrase with punctuation and the sentence case
     """
     def sentencize_phrase(self, phrase, eol, punc):
-        chosen_punc = self.qrandom(set(open(punc.keys().read().split())))
+        random = self.qrandom(len(punc))
+        chosen_punc = punc[random]
         phrase = phrase.split(' ' + eol)
         punctuated_phrase = chosen_punc.join(phrase)
 
@@ -93,7 +97,8 @@ class Stepper(object):
         string: the next word to be used in the phrase
     """
     def step(self, model, key):
-        return self.qrandom(set(open(model[key])))
+        random = self.qrandom(len(list(model[key])))
+        return list(model[key])[random]
 
     MAX_QUBITS = 16
 
@@ -106,17 +111,16 @@ class Stepper(object):
     def num_bits(self, n):
         return math.floor(math.log(n, 2)) + 1
 
-    def get_register_sizes(n, max_qubits):
+    def get_register_sizes(self, n, max_qubits):
         register_sizes = [max_qubits for i in range(int(n / max_qubits))]
         remainder = n % max_qubits
         return register_sizes if remainder == 0 else register_sizes + [remainder]
 
     def random_int(self, max):
         bits = ''
-        num = max - 1
         n_bits = self.num_bits(max - 1)
         # The max number of Qubits available need to be enough to encode the number
-        register_sizes = self.get_register_sizes(n_bits, self.MAX_QUBITS)
+        register_sizes = self.get_register_sizes(n_bits, 16)
         # print("Qubits "+ str(register_sizes))
 
         backend = get_aer_backend('qasm_simulator')
@@ -134,7 +138,7 @@ class Stepper(object):
             # print(sim_result)
             counts = sim_result.get_counts(qc)
             # print(counts)
-            bits += max.bit_from_counts(counts)
+            bits += self.bit_from_counts(counts)
         return int(bits, 2)
 
     # ------------
@@ -148,8 +152,9 @@ class Stepper(object):
         value = result
 
         normalized = (value - min) / (max - min)
-        # print(normalized)
+        # print(normalized
 
-        random = int(np.around(normalized * (limit - 1)))
-        print(random)
+        random = int(np.around(normalized * float(limit - 1)))
+
+        #print(random)
         return random
